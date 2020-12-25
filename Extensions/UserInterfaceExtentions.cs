a96857c4d156481f35bcc8bcc51e141ab16e8f26
AlexanderChunarev <44711132+AlexanderChunarev@users.noreply.github.com>
2020-12-25 17:33:44 +0200

    Update UserInterfaceExtentions.cs

--- Extensions/UserInterfaceExtentions.cs
+++ Extensions/UserInterfaceExtentions.cs
@@ -6,11 +6,6 @@ namespace AirportAPI.Extensions
     {
         public static IServiceCollection AddPresenters(this IServiceCollection services)
         {
-            // services.AddScoped<RegisterClientPresenter, RegisterClientPresenter>();
-            // services.AddScoped<Post.Application.Boundaries.Client.IOutputPort>(
-            //     x => x.GetRequiredService<RegisterClientPresenter>()
-            //     );
-
             return services;
         }
     }
