28de58d8d8060eac58e259d940bb69d36517d2bd
AlexanderChunarev <alexanderchunarov1999@gmail.com>
2020-12-26 17:49:48 +0200

    Create get flight details service

---
+++ Services/Flight/GetFlight/GetFlightDetailsService.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using AirportAPI.DapperDataAccess.Repositories.Flight;
+
+namespace AirportAPI.Services.Flight.GetFlight
+{
+    public class GetFlightDetailsService : IGetFlightDetailsService
+    {
+        private readonly IFlightRepository _flightRepository;
+        private readonly IOutputPort _outputPort;
+
+        public GetFlightDetailsService(IFlightRepository flightRepository, IOutputPort outputPort)
+        {
+            _flightRepository = flightRepository;
+            _outputPort = outputPort;
+        }
+
+        public async Task Execute(int id)
+        {
+            var createdFlight = await _flightRepository.GetById(id);
+            _outputPort.Ok(createdFlight);
+        }
+    }
+}
---
+++ Services/Flight/GetFlight/IGetFlightDetailsService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace AirportAPI.Services.Flight.GetFlight
+{
+    public interface IGetFlightDetailsService
+    {
+        public Task Execute(int id);
+    }
+}
