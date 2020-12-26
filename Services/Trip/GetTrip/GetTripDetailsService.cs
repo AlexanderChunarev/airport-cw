152ad373b1bee6a587b229c087a595064a31b80b
AlexanderChunarev <alexanderchunarov1999@gmail.com>
2020-12-26 20:38:03 +0200

    Create get trip details service

---
+++ Services/Trip/GetTrip/GetTripDetailsService.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using AirportAPI.DapperDataAccess.Repositories.Trip;
+
+namespace AirportAPI.Services.Trip.GetTrip
+{
+    public class GetTripDetailsService : IGetTripDetailsService
+    {
+        private readonly ITripRepository _tripRepository;
+        private readonly IOutputPort _outputPort;
+
+        public GetTripDetailsService(ITripRepository tripRepository, IOutputPort outputPort)
+        {
+            _tripRepository = tripRepository;
+            _outputPort = outputPort;
+        }
+
+        public async Task Execute(int id)
+        {
+            var trip = await _tripRepository.GetById(id);
+            _outputPort.Ok(trip);
+        }
+    }
+}
---
+++ Services/Trip/GetTrip/IGetTripDetailsService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace AirportAPI.Services.Trip.GetTrip
+{
+    public interface IGetTripDetailsService
+    {
+        public Task Execute(int id);
+    }
+}
