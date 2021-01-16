904fc9a71b54370fc5de22f0263af593a8d423d3
AlexanderChunarev <alexanderchunarov1999@gmail.com>
2021-01-16 21:57:29 +0200

    Dockerize app

--- DapperDataAccess/Migrations/Migration_202018122102.cs
+++ DapperDataAccess/Migrations/Migration_202018122102.cs
@@ -1,10 +1,18 @@
-﻿using FluentMigrator;
+﻿using System.IO;
+using FluentMigrator;
+using Microsoft.AspNetCore.Hosting;
 
 namespace AirportAPI.DapperDataAccess.Migrations
 {
     [Migration(202018122102)]
     public class Migration_202018122102 : Migration
     {
+        private IWebHostEnvironment _hostEnvironment;
+
+        public Migration_202018122102(IWebHostEnvironment environment) {
+            _hostEnvironment = environment;
+        }
+        
         public override void Up()
         {
             Create.Table("country")
@@ -42,7 +50,37 @@ namespace AirportAPI.DapperDataAccess.Migrations
                 .WithColumn("flight_code").AsString()
                 .WithColumn("plane_id").AsInt32().ForeignKey("plane", "id");
             
-            Execute.Script("DapperDataAccess/Migrations/initial_data.sql");
+            Execute.Sql(@"INSERT INTO country(name, code)
+            VALUES ('Italy', 'IT'),
+            ('China', 'CN'),
+            ('United Kingdom', 'GB'),
+            ('United Arab Emirates', 'AE'),
+            ('Spain', 'ES'),
+            ('Singapore', 'SG'),
+            ('United States', 'US'),
+            ('Germany', 'DE'),
+            ('France', 'FR');
+       
+            INSERT INTO airport(name,city,country_id)
+            VALUES ('Hartsfield Jackson Atlanta Intl','Atlanta',7),
+            ('Chicago Ohare Intl','Chicago',7),
+            ('Capital Intl','Beijing',2),
+            ('Heathrow','London', 3),
+            ('Charles De Gaulle','Paris', 9),
+            ('Los Angeles Intl','Los Angeles', 7),
+            ('Frankfurt Main','Frankfurt', 8),
+            ('Dallas Fort Worth Intl','Dallas-Fort Worth', 7),
+            ('John F Kennedy Intl','New York', 7),
+            ('Pudong','Shanghai', 2),
+            ('Changi Intl','6', 6),
+            ('Barcelona','Barcelona', 5),
+            ('Denver Intl','Denver', 7),
+            ('Miami Intl','Miami', 7),
+            ('Dubai Intl','Dubai', 4),
+            ('Gatwick','London', 3),
+            ('Baiyun Intl','Guangzhou', 2),
+            ('Fiumicino','Rome', 1);");
+           
         }
 
         public override void Down()
