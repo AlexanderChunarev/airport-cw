fbbfcafb35ac49e0c392972b3e99a4dd593c69ec
AlexanderChunarev <alexanderchunarov1999@gmail.com>
2021-01-17 21:44:28 +0200

    Dockerize app

--- DapperDataAccess/Migrations/Migration_202018122102.cs
+++ DapperDataAccess/Migrations/Migration_202018122102.cs
@@ -9,22 +9,23 @@ namespace AirportAPI.DapperDataAccess.Migrations
     {
         private IWebHostEnvironment _hostEnvironment;
 
-        public Migration_202018122102(IWebHostEnvironment environment) {
+        public Migration_202018122102(IWebHostEnvironment environment)
+        {
             _hostEnvironment = environment;
         }
-        
+
         public override void Up()
         {
             Create.Table("country")
                 .WithColumn("id").AsInt32().NotNullable().PrimaryKey().Identity()
                 .WithColumn("name").AsString()
                 .WithColumn("code").AsString();
-            
+
             Create.Table("airline")
                 .WithColumn("id").AsInt32().NotNullable().PrimaryKey().Identity()
                 .WithColumn("name").AsString()
                 .WithColumn("description").AsString().Nullable();
-            
+
             Create.Table("plane")
                 .WithColumn("id").AsInt32().NotNullable().PrimaryKey().Identity()
                 .WithColumn("name").AsString()
@@ -37,7 +38,7 @@ namespace AirportAPI.DapperDataAccess.Migrations
                 .WithColumn("latitude").AsDouble()
                 .WithColumn("longitude").AsDouble()
                 .WithColumn("country_id").AsInt32().ForeignKey("country", "id");
-            
+
             Create.Table("trip")
                 .WithColumn("id").AsInt32().NotNullable().PrimaryKey().Identity()
                 .WithColumn("airline_id").AsInt32().ForeignKey("airline", "id");
@@ -51,7 +52,7 @@ namespace AirportAPI.DapperDataAccess.Migrations
                 .WithColumn("arrive_date").AsDateTime()
                 .WithColumn("flight_code").AsString()
                 .WithColumn("plane_id").AsInt32().ForeignKey("plane", "id");
-            
+
             Execute.Sql(@"INSERT INTO country(name, code)
             VALUES ('Italy', 'IT'),
             ('China', 'CN'),
@@ -63,26 +64,25 @@ namespace AirportAPI.DapperDataAccess.Migrations
             ('Germany', 'DE'),
             ('France', 'FR');
        
-            INSERT INTO airport(name,city,country_id)
-            VALUES ('Hartsfield Jackson Atlanta Intl','Atlanta',7),
-            ('Chicago Ohare Intl','Chicago',7),
-            ('Capital Intl','Beijing',2),
-            ('Heathrow','London', 3),
-            ('Charles De Gaulle','Paris', 9),
-            ('Los Angeles Intl','Los Angeles', 7),
-            ('Frankfurt Main','Frankfurt', 8),
-            ('Dallas Fort Worth Intl','Dallas-Fort Worth', 7),
-            ('John F Kennedy Intl','New York', 7),
-            ('Pudong','Shanghai', 2),
-            ('Changi Intl','6', 6),
-            ('Barcelona','Barcelona', 5),
-            ('Denver Intl','Denver', 7),
-            ('Miami Intl','Miami', 7),
-            ('Dubai Intl','Dubai', 4),
-            ('Gatwick','London', 3),
-            ('Baiyun Intl','Guangzhou', 2),
-            ('Fiumicino','Rome', 1);");
-           
+            INSERT INTO airport(name,city,country_id, latitude, longitude)
+            VALUES ('Hartsfield Jackson Atlanta Intl','Atlanta',7, 33.636719,-84.428067),
+                ('Chicago Ohare Intl','Chicago',7, 41.978603,-87.904842),
+                ('Capital Intl','Beijing',2, 40.080111,116.584556),
+                ('Heathrow','London', 3, 51.4775,-0.461389),
+                ('Charles De Gaulle','Paris', 9, 49.012779,2.55),
+                ('Los Angeles Intl','Los Angeles', 7, 33.942536,-118.40807),
+                ('Frankfurt Main','Frankfurt', 8, 50.026421,8.543125),
+                ('Dallas Fort Worth Intl','Dallas-Fort Worth', 7, 32.896828,-97.037997),
+                ('John F Kennedy Intl','New York', 7, 40.639751,-73.778925),
+                ('Pudong','Shanghai', 2, 31.143378,121.805214),
+                ('Changi Intl','6', 6, 1.350189,103.994433),
+                ('Barcelona','Barcelona', 5, 41.297078,2.078464 ),
+                ('Denver Intl','Denver', 7, 39.861656,-104.67317),
+                ('Miami Intl','Miami', 7, 25.79325,-80.290556),
+                ('Dubai Intl','Dubai', 4, 25.252778,55.364444),
+                ('Gatwick','London', 3, 51.148056,-0.190278),
+                ('Baiyun Intl','Guangzhou', 2, 23.392436,113.298786),
+                ('Fiumicino','Rome', 1, 41.804475,12.250797);");
         }
 
         public override void Down()
--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,18 @@
 ﻿FROM mcr.microsoft.com/dotnet/core/sdk:3.1 AS build-env
 WORKDIR /app
 
- # Copy csproj and restore as distinct layers
- COPY *.csproj ./
- RUN dotnet restore
+# Copy csproj and restore as distinct layers
+COPY *.csproj ./
+RUN dotnet restore
 
 # Copy everything else and build
 COPY . ./
 
-#CMD ASPNETCORE_URLS=http://*:$PORT dotnet hashify
- RUN dotnet publish -c Release -o out
+RUN dotnet publish -c Release -o out
 
- # Build runtime image
- FROM mcr.microsoft.com/dotnet/core/aspnet:3.1
- WORKDIR /app
- COPY --from=build-env /app/out .
- ENTRYPOINT ["dotnet", "AirportAPI.dll", "mode=async"]
+# Build runtime image
+FROM mcr.microsoft.com/dotnet/core/aspnet:3.1
+WORKDIR /app
+COPY --from=build-env /app/out .
+
+CMD ASPNETCORE_URLS=http://*:5000 dotnet AirportAPI.dll
