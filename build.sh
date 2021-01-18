1c30aeffdff79429e999398086b58c6162d13da2
AlexanderChunarev <alexanderchunarov1999@gmail.com>
2021-01-18 19:47:07 +0200

    Fix CI

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 ﻿#!/bin/bash
-TAG=$CIRCLE_BRANCH-$CIRCLE_BUILD_NUM
-docker build --build-arg CONNECTION_STRING=$1 -t airport-cw-api:$TAG -f Dockerfile .
+TAG=$CIRCLE_BRANCH:$CIRCLE_BUILD_NUM
+docker build --build-arg CONNECTION_STRING=$1 -t $TAG -f Dockerfile .
