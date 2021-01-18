1148ad1a83364ecea6dc022ae60579db877235a9
AlexanderChunarev <alexanderchunarov1999@gmail.com>
2021-01-18 19:33:42 +0200

    Update configs

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 ﻿#!/bin/bash
 TAG=$CIRCLE_BRANCH-$CIRCLE_BUILD_NUM
-docker build --build-arg CONNECTION_STRING=$1 -t airport-cw-demo:$TAG -f Dockerfile .
+docker build --build-arg CONNECTION_STRING=$1 -t airport-cw-api:$TAG -f Dockerfile .
